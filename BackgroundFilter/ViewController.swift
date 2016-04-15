ea183561495e9dd7cfb2e80d9c915c8f667f54eb
Tiago-Rocha <tiago.aziz@gmail.com>
2016-04-15 19:06:51 +0200

    Fixed image load after select

--- BackgroundFilter/ViewController.swift
+++ BackgroundFilter/ViewController.swift
@@ -7,21 +7,25 @@
 //
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate  {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
-    //@IBOutlet weak var photoImageView: UIImageView!
-    
+    //Assign a var to the image
     @IBOutlet var photoImageView: UIImageView!
+    
     // Create a place to render the filtered image
     let context = CIContext(options: nil)
     
+    // Create an image picker constant
+    let imagePicker = UIImagePickerController()
+    
     @IBAction func importPhoto(sender: AnyObject) {
         NSLog("Button Pressed")
-      /*  let picker = UIImagePickerController()
-        picker.allowsEditing = true
-        picker.
-        presentViewController(picker, animated: true, completion: nil)
-    */}
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    //Apply filter function
     @IBAction func applyFilter(sender: AnyObject) {
         
         // Create an image to filter
@@ -44,62 +48,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate  {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-}/*
-
-class ViewController: UIViewController/*, UINavigationControllerDelegate, UIImagePickerController */ {
-//    class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
-    @IBOutlet weak var photoImageView: UIImageView!
-    
-    // Create a place to render the filtered image
-    let context = CIContext(options: nil)
-    
-    // Create ImportInstance
-    
-    @IBAction func applyFilter(sender: AnyObject) {
-        
-        // Create an image to filter
-        let inputImage = CIImage(image: photoImageView.image!)
-        
-        // Create a random color to pass to a filter
-        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
-        
-        // Create a filter to the image
-        let filteredImage = inputImage!.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
-        
-        // Render the filtered image
-        let renderedImage = context.createCGImage(filteredImage, fromRect: (filteredImage.extent))
-        
-        // Reflect the change back in the interface
-        photoImageView.image = UIImage(CGImage: renderedImage)
-        
-    }
-    /*
-    @IBAction func importPhoto() {
-     /*   let imagePicker = UIImagePickerController()
-        imagePicker.allowsEditing = false
-        imagePicker.sourceType = .PhotoLibrary
         imagePicker.delegate = self
-        presentViewController(imagePicker, animated: true, completion: nil)*/
-    }*/
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info :[String : AnyObject]) {
+        NSLog("sdfkjslfjslf")
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            photoImageView.contentMode = .ScaleAspectFit
+            photoImageView.image = pickedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
-*/
