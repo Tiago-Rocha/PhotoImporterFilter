278e35216d60887578ab1cbe0ce0a4dca1d71b71
Tiago-Rocha <tiago.aziz@gmail.com>
2016-04-15 15:10:22 +0200

    First commit

--- BackgroundFilter/ViewController.swift
+++ BackgroundFilter/ViewController.swift
@@ -5,13 +5,86 @@
 //  Created by Tiago Rocha on 15/04/16.
 //  Copyright © 2016 Tiago Rocha. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // Create a place to render the filtered image
+    let context = CIContext(options: nil)
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        
+        // Create an image to filter
+        let inputImage = CIImage(image: photoImageView.image!)
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Apply a filter to the image
+        let filteredImage = inputImage!.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Render the filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent)
+        
+        // Reflect the change back in the interface
+        photoImageView.image = UIImage(CGImage: renderedImage)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}/*
+import UIKit
+
+class ViewController: UIViewController/*, UINavigationControllerDelegate, UIImagePickerController */ {
+//    class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // Create a place to render the filtered image
+    let context = CIContext(options: nil)
+    
+    // Create ImportInstance
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        
+        // Create an image to filter
+        let inputImage = CIImage(image: photoImageView.image!)
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Create a filter to the image
+        let filteredImage = inputImage!.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Render the filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: (filteredImage.extent))
+        
+        // Reflect the change back in the interface
+        photoImageView.image = UIImage(CGImage: renderedImage)
+        
+    }
+    /*
+    @IBAction func importPhoto() {
+     /*   let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)*/
+    }*/
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,4 +95,4 @@ class ViewController: UIViewController {
 
 
 }
-
+*/
